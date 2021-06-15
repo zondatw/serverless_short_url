99ddf68c9590c562f0333c16ddcfa722ce8bdff3
zonda yang <u226699@gmail.com>
2021-06-15 21:29:49 +0800

    Add CORS

--- statisticsserver/router.go
+++ statisticsserver/router.go
@@ -4,10 +4,13 @@ import (
 	"context"
 
 	"cloud.google.com/go/firestore"
+	"github.com/zondatw/serverless_short_url/statisticsserver/middleware"
 	"github.com/zondatw/serverless_short_url/statisticsserver/shorturl"
 )
 
 func initRoute(ctx context.Context, client *firestore.Client) {
+	router.Use(middleware.CORSMiddleware)
+
 	api := router.Group("/api")
 	shorturl.Route(ctx, api, client)
 }
