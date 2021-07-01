899714ad23a9f47d9577b53b0d3fa5819285bbbf
zonda yang <u226699@gmail.com>
2021-07-01 23:19:47 +0800

    Modify hash.js: forward apikey to server sider render

--- short-url-frontend/pages/Details/[hash].js
+++ short-url-frontend/pages/Details/[hash].js
@@ -1,28 +1,56 @@
+import Error from 'next/error'
+import Cookies from 'cookies'
 import getConfig from 'next/config'
 
 import Layout from '../../components/layout'
+import { useAuthContext } from "../../context/auth"
 import ShortUrlDetail from '../../components/shorUrlDetail'
 
 const { serverRuntimeConfig, publicRuntimeConfig } = getConfig()
 
 
 export async function getServerSideProps(context) {
+  var errorCode = false
   const { hash } = context.query
   var apiURL = process.env.apiURL || publicRuntimeConfig.apiURL
+  const cookies = new Cookies(context.req, context.res)
+  var apikey = cookies.get('apikey')
+  var headers = {}
+  if (apikey != null) {
+    headers["Authorization"] = apikey
+  } else {
+    apikey = ""
+  }
 
-  const res = await fetch(`${apiURL}api/shorturl/${hash}`)
+  const res = await fetch(`${apiURL}api/shorturl/${hash}`, {headers: headers})
   const data = await res.json()
+
+  if ("error" in data) {
+    errorCode = 403;
+  }
 
   return {
     props: {
+      errorCode: errorCode,
       hash: hash,
       data: data,
+      isSignIn: apikey != "",
     }
   }
 }
 
 
-export default function Detail({hash, data}) {
+export default function Detail({errorCode, hash, data, isSignIn}) {
+  if (errorCode) {
+    return <Error statusCode={errorCode} />
+  }
+
+  const {authSignIn, authSignOut} = useAuthContext()
+  if (isSignIn) {
+    authSignIn()
+  } else {
+    authSignOut()
+  }
 
   return (
     <Layout title={`Detail ${hash}`}>
